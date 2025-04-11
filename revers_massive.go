c29c0a014066561afeeecc03870d3f4280770152
bezggod <bezgubenko243@gmail.com>
2025-04-11 18:09:41 +0300

    task reserve massive ready

--- revers_massive.go
+++ revers_massive.go
@@ -3,12 +3,12 @@ package main //Реализуйте функцию, которая приним�
 import "fmt"
 
 func main() {
-	reversed := reverse()
+	arr := [5]int{1, 2, 3, 4, 5}
+	reversed := reverse(arr)
 	fmt.Println(reversed)
 }
 
-func reverse() []int {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+func reverse(arr [5]int) [5]int {
 	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
