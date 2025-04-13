8709e859debea1509b7555f76cd5cc4283ae7da7
bezggod <bezgubenko243@gmail.com>
2025-04-13 14:55:34 +0300

    task average massive ready

---
+++ massive_average.go
@@ -0,0 +1,17 @@
+package main // Напишите функцию, которая вычисляет среднее значение элементов массива и возвращает его.
+
+import "fmt"
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	avg := average(array)
+	fmt.Println(avg)
+}
+
+func average(array [5]int) int {
+	sum := 0
+	for _, value := range array {
+		sum += value
+	}
+	return sum / len(array)
+}
